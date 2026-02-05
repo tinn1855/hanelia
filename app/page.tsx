fb5693f887b3c505e7e70985b6c477e4b2ae7404
Tin Nguyen <tinn1855@gmail.com>
2026-02-05 21:30:19 +0700

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Footer } from "@/components/common/footer";
 import { Header } from "@/components/common/header";
+import { HeroSection } from "@/components/molecules/hero-section";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <p>home</p>
+        <HeroSection />
       </main>
       <Footer />
     </>
